f624b7248f394b17d52f148275a180da3910bcbc
Horacio Araiza Gonzalez <69490905+Horacio-bit@users.noreply.github.com>
2021-06-07 10:30:10 -0700

    commit on 'Master'

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Course;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -9,6 +9,10 @@ class HomeController extends Controller
     //
     public function __invoke()
     {
-        
+        $courses = Course::where('status','3')->get();
+        return $courses;
+    
+        return view('welcome');
+
     }
 }
